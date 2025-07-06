7a61f0fffcc8e1bb5ceacd2a6e1c647caabb0807
IdanSSch <imsdev2006@icloud.com>
2025-07-05 19:37:18 -0700

    Okay, I'm not making one commit per function
    square, isEven, isOdd, multiply, floorMod

--- math.js
+++ math.js
@@ -1,4 +1,24 @@
 function double(n /* : number */) { // wow... if only there was a way to declare types, like... a type script!!!
+    return n * 2;
+}
+
+function square(n) {
     return n * n;
 }
 
+function isEven(n) {
+    return n % 2 === 0;
+}
+
+function isOdd(n) {
+    return n % 2 !== 0;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+// I have needed this in the past for... things. I'm pretty sure this one's wrong, though.
+function floorMod(a, n) {
+    return ((a % n) + n) % n;
+}
